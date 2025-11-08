fcbfc63ec65a5ffa142644957f94c72b3301ffcc
kiran-kumarb <kirankumarbdoddamani@gmail.com>
2025-11-08 11:38:36 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "kirankumarb7@K",
+  password: "\\\\\\\\\\",
   database: "food_drone"
 });
 
